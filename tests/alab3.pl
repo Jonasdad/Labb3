90f5505011d723fb9b60255815b74127ca809bd6
Jonas Dåderman <jonasdad@kth.se>
2023-11-30 22:12:05 +0100

    hjälp

--- tests/alab3.pl
+++ tests/alab3.pl
@@ -21,19 +21,19 @@ verify(Input) :-
 ok(_,_,[],_,_).
 ok(T,L,[H|R],U,F):-check(T,L,H,U,F),ok(T,L,R,U,F).
 
-check(_, L, S, [], X):- member([S, List], L), member(X, List),write('literal').
-check(_, L, S, [], neg(X)):- member([S, List], L), \+member(X, List),write('neg').
+check(_, L, S, [], X):- member([S, List], L), member(X, List).
+check(_, L, S, [], neg(X)):- member([S, List], L), \+member(X, List).
 % And
-check(T, L, S, U, and(F,G)):- check(T, L, S, [], F), check(T, L, S, [], G),write('And').
+check(T, L, S, U, and(F,G)):- check(T, L, S, [], F), check(T, L, S, [], G).
 % Or
-check(T, L, S, U, or(F,G)):- check(T, L, S, [], F),write('Or(F)').
-check(T, L, S, U, or(F,G)):- check(T, L, S, [], G),write('Or(G)').
+check(T, L, S, U, or(F,G)):- check(T, L, S, [], F).
+check(T, L, S, U, or(F,G)):- check(T, L, S, [], G).
 % AX
 check(T, L, S, [], ax(F)) :- member([S, List], T),
-                        ok(T,L,List,[], F),write('Ax').
+                        ok(T,L,List,[], F).
 % EX
 check(T, L, S, [], ex(F)) :- member([S, List], T), 
-                        member(New, List), check(T,L,New,[],F),write('Ex').
+                        member(New, List), check(T,L,New,[],F).
 % AG - For every single path from S in the Graph, F is ALWAYS true. Action: Implement DFS.
 
 
@@ -45,7 +45,7 @@ check(_, _, S, U, ag(_)) :- member(S, U). % Cycle detection
 check(T, L, S, U, ag(F)) :- \+member(S,U),
     check(T, L, S, [], F),
     member([S,List], T),
-    ok(T,L,P,[S|U],ag(F)),write('Ag').
+    ok(T,L,List,[S|U],ag(F)).
 
 
 % EG - For some path from S, F becomes true and stays true from that point on.
@@ -53,19 +53,19 @@ check(T, L, S, U, ag(F)) :- \+member(S,U),
 check(_, _, S, U, eg(_)):- member(S, U).
 check(T,L,S,U,eg(F)):- \+member(S,U),
                         check(T,L,S,[],F),
-                        member([S,P],T),
-                        member(New,P),
-                        check(T,L,New,[S|U],eg(F)),write('Eg').
+                        member([S,List],T),
+                        member(New,List),
+                        check(T,L,New,[S|U],eg(F)).
 
 
 % EF - For some future path P from S, F becomes true. 
 check(T,L,S,U,ef(F)):- \+member(S,U), check(T,L,S,[],F).
-check(T,L,S,U,ef(F)):- \+member(S,U), member([S,P],T),
-                        member(S2,P),
-                        check(T,L,S2,[S|U],ef(F)),write('ef').
+check(T,L,S,U,ef(F)):- \+member(S,U), member([S,List],T),
+                        member(S2,List),
+                        check(T,L,S2,[S|U],ef(F)).
 % AF - For all future paths from S, F becomes true.
 check(T,L,S,U,af(F)):- \+member(S,U),
                         check(T,L,S,[],F).
 check(T,L,S,U,af(F)):- \+member(S,U),
                         member([S,List],T),
-                        ok(T,L,List,[S|U], af(F)),write('af').
+                        ok(T,L,List,[S,U], af(F)).
